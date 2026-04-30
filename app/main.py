b115fbe7725cc7e2a91ff87c56c37698cc11b7de
unknown <victoriiawww@gmail.com>
2026-04-30 23:25:10 +0300

    Add FastAPI endpoints and database models

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.database import engine
-from app import models
+from app import models, schemas
+
+from app.database import SessionLocal
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Fitness Tracker API",
@@ -8,20 +14,52 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def home():
     return {"message": "Fitness Tracker API is running"}
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
 
-@app.get("/db-check")
-def db_check():
-    try:
-        with engine.connect() as connection:
-            return {"database": "connected"}
-    except Exception as e:
-        return {"database": "error", "details": str(e)}
-    
-models.Base.metadata.create_all(bind=engine)
+@app.post("/users", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(
+        name=user.name,
+        age=user.age,
+        email=user.email,
+        password=user.password
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+@app.get("/users", response_model=list[schemas.User])
+def get_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    return db.query(models.User).filter(models.User.user_id == user_id).first()
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.user_id == user_id).first()
+
+    if user:
+        db.delete(user)
+        db.commit()
+        return {"message": "User deleted"}
+
+    return {"error": "User not found"}
+
